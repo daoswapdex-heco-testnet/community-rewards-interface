3b01687784fb337bade8319ed68c6fc1c6a460b5
daoswap002 <daoswap@daoswap.cc>
2021-07-25 16:32:29 +0800

    init project

---
+++ src/layouts/home/AppBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-app-bar
+      id="home-app-bar"
+      app
+      elevation="1"
+      elevate-on-scroll
+      height="80"
+    >
+      <base-img
+        :src="
+          require(`@/assets/logo-${
+            $vuetify.theme.isDark ? 'dark' : 'light'
+          }.png`)
+        "
+        contain
+        max-width="46"
+        width="100%"
+      />
+
+      <!-- 多语言切换 -->
+      <!-- <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">
+            <v-icon>mdi-translate</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="item in localeList" :key="item.locale" link>
+            <v-list-item-title
+              v-text="item.title"
+              @click="changeLang(item.locale)"
+            ></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu> -->
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeAppBar",
+
+  data: () => ({
+    // 当前包含语言
+    localeList: [
+      {
+        locale: "zh",
+        title: "中文"
+      },
+      {
+        locale: "en",
+        title: "English"
+      }
+    ]
+  }),
+  methods: {
+    changeLang(locale) {
+      this.$i18n.locale = locale;
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+#home-app-bar
+  .v-tabs-slider
+    max-width: 24px
+    margin: 0 auto
+
+  .v-tab
+    &::before
+      display: none
+</style>
